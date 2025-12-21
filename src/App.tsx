5efacd1d235fa34642d6ef2a460264fc8046b2de
Abhishek Rathore <abhishekrathore94500@gmail.com>
2025-12-21 18:24:52 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Pricing from './pages/Pricing'
+import Projects from './pages/Projects'
+import MyProjects from './pages/MyProjects'
+import Preview from './pages/Preview'
+import Community from './pages/Community'
+import View from './pages/View'
+import Navbar from './components/Navbar'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+   <div className='overflow-hidden'>
+    <Navbar/>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/pricing' element={<Pricing />} />
+      <Route path='/projects/:projectId' element={<Projects />} />
+      <Route path='/projects' element={<MyProjects />} />
+      <Route path='/preview/:projectId' element={<Preview />} />
+      <Route path='/preview/:projectId/:versionId' element={<Preview />} />
+      <Route path='/community' element={<Community />} />
+      <Route path='/view/:projectId' element={<View />} />
+    </Routes>
+   </div>
+  )
+}
+
+export default App
